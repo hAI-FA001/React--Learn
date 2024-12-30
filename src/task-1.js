255e1b6bd76772fab3279f044c781aa6aa3a6f93
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-12-30 21:24:32 +0500

    refactor into single Book, remove inline css

--- src/task-1.js
+++ src/task-1.js
@@ -1,24 +1,9 @@
-const Title = () => {
-  return <h2>Onyx Storm</h2>
-}
-const Author = () => {
-  const inlineH4Style = {
-    color: '#617d98',
-    fontSize: '0.75',
-    marginTop: '0.5rem',
-  }
-  return <h4 style={inlineH4Style}>Rebecca Yarros</h4>
-}
-const Image = () => {
-  return <img src="./images/book-cover.jpg" alt="Onyx Storm" />
-}
-
 const Book = () => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src="./images/book-cover.jpg" alt="Onyx Storm" />
+      <h2>Onyx Storm</h2>
+      <h4>Rebecca Yarros</h4>
     </article>
   )
 }
