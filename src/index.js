b510a14c471ae5b7a65668865298118b471bbfc9
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-12-30 21:09:37 +0500

    add CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import './index.css'
+
 import { BookList } from './task-1'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
--- src/task-1.js
+++ src/task-1.js
@@ -15,7 +15,7 @@ const Image = () => {
 
 const Book = () => {
   return (
-    <article>
+    <article className="book">
       <Image />
       <Title />
       <Author />
@@ -25,7 +25,7 @@ const Book = () => {
 
 export const BookList = () => {
   return (
-    <section>
+    <section className="booklist">
       <Book />
       <Book />
       <Book />
