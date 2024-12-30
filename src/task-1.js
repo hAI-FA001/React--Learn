e7be51cfc21676a1ebbf7ab9fc9be239b27cab80
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-12-30 21:20:55 +0500

    inline css style - alt preference

--- src/task-1.js
+++ src/task-1.js
@@ -2,11 +2,12 @@ const Title = () => {
   return <h2>Onyx Storm</h2>
 }
 const Author = () => {
-  return (
-    <h4 style={{ color: '#617d98', fontSize: '0.75', marginTop: '0.5rem' }}>
-      Rebecca Yarros
-    </h4>
-  )
+  const inlineH4Style = {
+    color: '#617d98',
+    fontSize: '0.75',
+    marginTop: '0.5rem',
+  }
+  return <h4 style={inlineH4Style}>Rebecca Yarros</h4>
 }
 const Image = () => {
   return <img src="./images/book-cover.jpg" alt="Onyx Storm" />
