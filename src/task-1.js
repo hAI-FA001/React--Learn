ce913ed0767fb7f7aa49618ab73396f52bdc63e9
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-12-30 21:20:05 +0500

    inline css in component

--- src/task-1.js
+++ src/task-1.js
@@ -2,7 +2,11 @@ const Title = () => {
   return <h2>Onyx Storm</h2>
 }
 const Author = () => {
-  return <h4>Rebecca Yarros</h4>
+  return (
+    <h4 style={{ color: '#617d98', fontSize: '0.75', marginTop: '0.5rem' }}>
+      Rebecca Yarros
+    </h4>
+  )
 }
 const Image = () => {
   return <img src="./images/book-cover.jpg" alt="Onyx Storm" />
