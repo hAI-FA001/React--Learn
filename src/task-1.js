4a35c5b418f2aeb502c9eaa6219cc5ed0a6aeede
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-12-30 21:47:43 +0500

    JS in JSX

--- src/task-1.js
+++ src/task-1.js
@@ -1,9 +1,11 @@
 const Book = () => {
+  const author = 'Rebecca Yarros'
+  const title = 'Onyx Storm'
   return (
     <article className="book">
       <img src="./images/book-cover.jpg" alt="Onyx Storm" />
-      <h2>Onyx Storm</h2>
-      <h4>Rebecca Yarros</h4>
+      <h2>{title}</h2>
+      <h4>{author.toUpperCase()}</h4>
     </article>
   )
 }
