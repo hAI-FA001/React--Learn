f9aa153004d7c7f851d61d94e7f78691c4bc0179
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-12-31 22:15:18 +0500

    local images in src for optimization

--- src/books.js
+++ src/books.js
@@ -1,15 +1,20 @@
+import bookCover1 from './images/book-cover.jpg'
+import bookCover2 from './images/book-cover-2.webp'
+
 export const books = [
   {
     author: 'Rebecca Yarros',
     title: 'Onyx Storm',
-    img: './images/book-cover.jpg',
+    // img: './images/book-cover.jpg',
+    img: bookCover1,
     id: 1,
   },
 
   {
     author: 'James Clear',
     title: 'Atomic Habits',
-    img: './images/book-cover-2.webp',
+    // img: './images/book-cover-2.webp',
+    img: bookCover2,
     id: 2,
   },
 ]
