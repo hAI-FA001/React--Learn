f460063c763369a4ac2e2b5d2da2ef63b00adaa2
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-12-31 22:43:49 +0500

    boko number challenge

--- src/Book.js
+++ src/Book.js
@@ -1,9 +1,10 @@
-const Book = ({ img, title, author, children }) => {
+const Book = ({ img, title, author, number, children }) => {
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author.toUpperCase()}</h4>
+      <span className="number">{`#${number}`}</span>
       {children}
     </article>
   )
