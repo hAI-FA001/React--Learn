6f3675ca8a9c8a57120d505321a6cf92c9292309
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-12-31 22:47:06 +0500

    h1 + html title challenge

--- src/BookList.js
+++ src/BookList.js
@@ -3,22 +3,25 @@ import { books } from './books'
 
 export const BookList = () => {
   return (
-    <section className="booklist">
-      {books.map((book, idx) =>
-        idx % 2 === 0 ? (
-          <Book key={book.id} {...book} />
-        ) : (
-          <Book key={book.id} {...book}>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              numquam necessitatibus accusantium eius quae magni, suscipit rem
-              commodi facilis quidem est quis voluptatum hic tempore, in tenetur
-              blanditiis ducimus asperiores.
-            </p>
-            <button onClick={() => alert('CLICKED BUTTON')}>CLICK</button>
-          </Book>
-        )
-      )}
-    </section>
+    <>
+      <h1>amazon best sellers</h1>
+      <section className="booklist">
+        {books.map((book, idx) =>
+          idx % 2 === 0 ? (
+            <Book key={book.id} {...{ ...book, number: idx + 1 }} />
+          ) : (
+            <Book key={book.id} {...book} number={idx + 1}>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Mollitia numquam necessitatibus accusantium eius quae magni,
+                suscipit rem commodi facilis quidem est quis voluptatum hic
+                tempore, in tenetur blanditiis ducimus asperiores.
+              </p>
+              <button onClick={() => alert('CLICKED BUTTON')}>CLICK</button>
+            </Book>
+          )
+        )}
+      </section>
+    </>
   )
 }
