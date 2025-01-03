b94a2b8a835e0b300d8a5b2bfefb9899827d0493
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-04 02:22:45 +0500

    state updates, auto-batching

--- Advanced/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ Advanced/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,32 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
-};
+  const [name, setName] = useState('Peter')
+  const [age, setAge] = useState(24)
+  const [hobby, setHobby] = useState('Read Books')
 
-export default UseStateObject;
+  return (
+    <>
+      <h2>useState Object Example</h2>
+      <div>
+        <h4>Name: {name}</h4>
+        <h4>Age: {age}</h4>
+        <h4>Hobby: {hobby}</h4>
+      </div>
+      <button
+        type="button"
+        className="btn"
+        onClick={() => {
+          // react will perform auto-batching
+          setName('John')
+          setAge(28)
+          setHobby('Scream At The Computer')
+        }}
+      >
+        Change Person
+      </button>
+    </>
+  )
+}
+
+export default UseStateObject
