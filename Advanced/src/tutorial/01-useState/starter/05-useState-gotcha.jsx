9e789c20e99357a5c9e191f7774dcfcfcedfd830
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-04 02:36:04 +0500

    set*() gotcha: not synchronous, soln: pass function

--- Advanced/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ Advanced/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,29 @@
-const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
-};
+import { useState } from 'react'
 
-export default UseStateGotcha;
+const UseStateGotcha = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>useState "gotcha"</h2>
+      <h4>State: {count}</h4>
+      <button
+        type="button"
+        className="btn"
+        onClick={() => {
+          // setCount(count + 1)
+          // console.log(count)
+
+          setCount((prevCount) => {
+            const newState = prevCount + 1
+            return newState
+          })
+        }}
+      >
+        Increment
+      </button>
+    </>
+  )
+}
+
+export default UseStateGotcha
