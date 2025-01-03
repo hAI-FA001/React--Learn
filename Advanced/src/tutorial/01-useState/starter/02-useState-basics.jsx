2a039bfeda8a16a67d665dcf7add21859df5e58a
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-04 01:51:54 +0500

    using useState for re-renders + preserving values

--- Advanced/src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ Advanced/src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,30 @@
-const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
-};
+import { useState } from 'react'
 
-export default UseStateBasics;
+const UseStateBasics = () => {
+  console.log(useState())
+  console.log(useState('hello'))
+  console.log(useState('hello')[0])
+  console.log(useState('hello')[1])
+
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>useState Basics</h2>
+      <h3>{count}</h3>
+      <button
+        type="button"
+        className="btn"
+        onClick={(e) => {
+          setCount(count + 1)
+          // setCount(count + count.toString()) // turns into different dtype
+          console.log(count)
+        }}
+      >
+        Increment
+      </button>
+    </>
+  )
+}
+
+export default UseStateBasics
