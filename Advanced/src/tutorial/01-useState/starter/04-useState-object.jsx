485e44443f76c88fabaa5bafe19a4a669c74b7d2
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-04 02:27:45 +0500

    refactor, multiple states -> 1 object

--- Advanced/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ Advanced/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,26 +1,33 @@
 import { useState } from 'react'
 
 const UseStateObject = () => {
-  const [name, setName] = useState('Peter')
-  const [age, setAge] = useState(24)
-  const [hobby, setHobby] = useState('Read Books')
+  // const [name, setName] = useState('Peter')
+  // const [age, setAge] = useState(24)
+  // const [hobby, setHobby] = useState('Read Books')
+  const [person, setPerson] = useState({
+    name: 'Peter',
+    age: 24,
+    hobby: 'Read Books',
+  })
 
   return (
     <>
       <h2>useState Object Example</h2>
       <div>
-        <h4>Name: {name}</h4>
-        <h4>Age: {age}</h4>
-        <h4>Hobby: {hobby}</h4>
+        <h4>Name: {person.name}</h4>
+        <h4>Age: {person.age}</h4>
+        <h4>Hobby: {person.hobby}</h4>
       </div>
       <button
         type="button"
         className="btn"
         onClick={() => {
           // react will perform auto-batching
-          setName('John')
-          setAge(28)
-          setHobby('Scream At The Computer')
+          // setName('John')
+          // setAge(28)
+          // setHobby('Scream At The Computer')
+
+          setPerson({ name: 'John', age: 28, hobby: 'Scream At The Computer' })
         }}
       >
         Change Person
