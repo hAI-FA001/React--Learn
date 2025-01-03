0653d80ace245c73a076b4c2be4dfae19b7db6c3
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-04 02:13:26 +0500

    useState with array + deleting/clearing

--- Advanced/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ Advanced/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,55 @@
-const UseStateArray = () => {
-  return <h2>useState array example</h2>;
-};
+import { useState } from 'react'
+import { data } from '../../../data'
 
-export default UseStateArray;
+const UseStateArray = () => {
+  const [people, setPeople] = useState(data)
+
+  const removeSingleItem = (removeThis) => {
+    // people.splice(
+    //   people.findIndex((item) => item.id == removeThis.id),
+    //   1
+    // )
+    // setPeople(people)
+    setPeople(people.filter((item) => item.id !== removeThis.id))
+  }
+
+  const clearList = () => {
+    setPeople([])
+  }
+
+  return (
+    <>
+      <h2>useState Array Example</h2>
+      {people.map((item) => (
+        <div
+          key={item.id}
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            width: '25%',
+            margin: '1rem auto',
+          }}
+        >
+          <span>{item.name.toUpperCase()}</span>
+          <button
+            type="button"
+            className="btn"
+            onClick={() => removeSingleItem(item)}
+          >
+            Delete
+          </button>
+        </div>
+      ))}
+      <button
+        type="button"
+        className="btn"
+        style={{ marginTop: '2rem' }}
+        onClick={clearList}
+      >
+        Clear List
+      </button>
+    </>
+  )
+}
+
+export default UseStateArray
