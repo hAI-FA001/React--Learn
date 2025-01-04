ebef853339e362019a1428ef76f0ba1381d7abf5
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-05 00:33:57 +0500

    order matters (user obj destructure), place fetchData outside + do not pass to dependency array

--- Advanced/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ Advanced/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,31 +1,31 @@
 import { useEffect, useState } from 'react'
-const url = 'https://api.github.com/users/QuincyLarsons'
+const url = 'https://api.github.com/users/QuincyLarson'
 
 const MultipleReturnsFetchData = () => {
   const [isLoading, setIsLoading] = useState(true)
   const [isError, setIsError] = useState(false)
   const [user, setUser] = useState(null)
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const resp = await fetch(url)
+  const fetchData = async () => {
+    try {
+      const resp = await fetch(url)
 
-        if (!resp.ok) {
-          setIsError(true)
-          return
-        }
-
-        const fetchedUser = await resp.json()
-        console.log(fetchedUser)
-        setUser(fetchedUser)
-      } catch (error) {
-        console.log(error)
+      if (!resp.ok) {
         setIsError(true)
-      } finally {
-        setIsLoading(false)
+        return
       }
+
+      const fetchedUser = await resp.json()
+      console.log(fetchedUser)
+      setUser(fetchedUser)
+    } catch (error) {
+      console.log(error)
+      setIsError(true)
+    } finally {
+      setIsLoading(false)
     }
+  }
+  useEffect(() => {
     fetchData()
   }, [])
 
@@ -34,17 +34,18 @@ const MultipleReturnsFetchData = () => {
   } else if (isError) {
     return <h1>Error occured</h1>
   } else {
+    const { avatar_url, login, name, company, bio } = user
     return (
       <>
         <h2>Fetch Data</h2>
         <img
-          src={user.avatar_url}
+          src={avatar_url}
           style={{ width: '150px', borderRadius: '25px' }}
-          alt={user.login}
+          alt={login}
         />
-        <h3>{user.name}</h3>
-        <h4>At {user.company}</h4>
-        <p>{user.bio}</p>
+        <h3>{name}</h3>
+        <h4>At {company}</h4>
+        <p>{bio}</p>
       </>
     )
   }
