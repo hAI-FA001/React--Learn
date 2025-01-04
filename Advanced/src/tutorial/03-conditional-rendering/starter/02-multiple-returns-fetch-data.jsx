837cfafd83e3429fb24174b32da32fcb08d9f4e6
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-05 00:15:36 +0500

    multiple returns fetch data (loading, error, success)

--- Advanced/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ Advanced/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,7 +1,46 @@
-import { useEffect, useState } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import { useEffect, useState } from 'react'
+const url = 'https://api.github.com/users/QuincyLarson'
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
-};
-export default MultipleReturnsFetchData;
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const resp = await fetch(url)
+        const fetchedUser = await resp.json()
+        console.log(fetchedUser)
+        setUser(fetchedUser)
+      } catch (error) {
+        console.log(error)
+        setIsError(true)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchData()
+  }, [])
+
+  if (isLoading) {
+    return <h1>Loading...</h1>
+  } else if (isError) {
+    return <h1>Error occured</h1>
+  } else {
+    return (
+      <>
+        <h2>Fetch Data</h2>
+        <img
+          src={user.avatar_url}
+          style={{ width: '150px', borderRadius: '25px' }}
+          alt={user.login}
+        />
+        <h3>{user.name}</h3>
+        <h4>At {user.company}</h4>
+        <p>{user.bio}</p>
+      </>
+    )
+  }
+}
+export default MultipleReturnsFetchData
