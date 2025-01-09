3ac2b6ec6dc857735d3a5300a0d4821d200f6271
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-09 09:52:50 +0500

    cleanup function in useEffect

--- Advanced/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ Advanced/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -3,13 +3,26 @@ import { useEffect, useState } from 'react'
 const SecondComponent = () => {
   useEffect(() => {
     console.log('useEffect invoked')
+
+    const intvID = setInterval(() => {
+      console.log('Interval callback')
+    }, 1000)
+
+    return () => {
+      clearInterval(intvID)
+      console.log('Inside cleanup function')
+    }
   }, [])
 
   return <p>Second Component Toggled</p>
 }
 
 const CleanupFunction = () => {
-  const [value, setValue] = useState(true)
+  const [value, setValue] = useState(false)
+
+  useEffect(() => {
+    console.log('First render of <CleanupFunction />')
+  }, [])
 
   return (
     <>
