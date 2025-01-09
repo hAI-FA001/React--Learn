2e6d12c34a332b46a0bb6ef55826c50594e5c7fa
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-09 09:33:55 +0500

    toggle challenge

--- Advanced/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ Advanced/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,26 @@
-const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
-};
+import { useState } from 'react'
 
-export default ToggleChallenge;
+const ToggleChallenge = () => {
+  const [state, setState] = useState(false)
+
+  return (
+    <>
+      <h2>toggle challenge</h2>
+      <button
+        type="button"
+        className="btn"
+        onClick={() => {
+          setState(!state)
+        }}
+      >
+        Toggle State
+      </button>
+
+      <div>{state ? <MyCompo /> : 'State is False'}</div>
+    </>
+  )
+}
+
+const MyCompo = () => <h3>Rendered Component</h3>
+
+export default ToggleChallenge
