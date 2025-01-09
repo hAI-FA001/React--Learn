9c35c2b1c44574fc1e80af3d889684d616a4c1da
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-09 09:07:21 +0500

    don't place hooks conditionally

--- Advanced/src/tutorial/03-conditional-rendering/starter/03-hooks-rule.jsx
+++ Advanced/src/tutorial/03-conditional-rendering/starter/03-hooks-rule.jsx
@@ -1,20 +1,44 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 
 const Example = () => {
-  const [condition, setCondition] = useState(true);
+  const [condition, setCondition] = useState(true)
   if (condition) {
     // won't work
-    const [state, setState] = useState(false);
+    const [state, setState] = useState(false)
+    console.log('MY STATE', state)
   }
 
-  // if (condition) {
-  //   return <h2>Hello There</h2>;
-  // }
-  // // this will also fail
-  // useEffect(() => {
-  //   console.log('hello there');
-  // }, []);
-  return <h2>example</h2>;
-};
+  if (!condition) {
+    return <h2>Hello There</h2>
+  }
+  // this will also fail
+  useEffect(() => {
+    console.log('hello there')
+  }, [])
+  return (
+    <>
+      <h2>example</h2>
+      <button
+        type="button"
+        className="btn"
+        onClick={() => {
+          setCondition(!condition)
+        }}
+      >
+        Toggle Condition
+      </button>
+      <br />
+      <button
+        type="button"
+        className="btn"
+        onClick={() => {
+          setState(!state)
+        }}
+      >
+        Toggle State
+      </button>
+    </>
+  )
+}
 
-export default Example;
+export default Example
