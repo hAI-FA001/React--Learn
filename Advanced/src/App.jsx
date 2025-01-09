14b519d6eb5e43776c00096ba5220caf0a8ccf2e
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-09 11:25:28 +0500

    project structure - another approach, Pages directory

--- Advanced/src/App.jsx
+++ Advanced/src/App.jsx
@@ -1,9 +1,11 @@
-import Navbar from './tutorial/04-project-structure/starter/Navbar'
+import About from './tutorial/04-project-structure/starter/Pages/About'
+import Home from './tutorial/04-project-structure/starter/Pages/Home'
 
 function App() {
   return (
     <div className="container">
-      <Navbar />
+      <Home />
+      <About />
     </div>
   )
 }
