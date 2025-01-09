05ff92bb0f11b6c0ab37b727badd9cb3ea6b2229
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-09 09:18:04 +0500

    short circuit evaluation + truthy, falsey values

--- Advanced/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ Advanced/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,46 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
-};
-export default ShortCircuitOverview;
+  const [a, setA] = useState('')
+  const [b, setB] = useState(undefined)
+
+  console.log(a, b)
+
+  return (
+    <>
+      <h2>short circuit overview</h2>
+      <button
+        type="button"
+        className="btn"
+        onClick={() => {
+          if (a) {
+            setA('')
+          } else {
+            setA('non empty')
+          }
+        }}
+      >
+        Toggle A
+      </button>
+      <br />
+      <button
+        type="button"
+        className="btn"
+        onClick={() => {
+          if (b) {
+            setB(0)
+          } else {
+            setB(-1)
+          }
+        }}
+      >
+        Toggle B
+      </button>
+      <br />
+      OR: {a || b}
+      <br />
+      AND: {a && b}
+    </>
+  )
+}
+export default ShortCircuitOverview
