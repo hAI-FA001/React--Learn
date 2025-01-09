ae308cd560eff167cb0e407a43dc875d7865b7e5
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-09 09:44:35 +0500

    useEffect + conditional rendering gotcha (initial render runs more than once)

--- Advanced/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ Advanced/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,25 @@
-const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
-};
+import { useEffect, useState } from 'react'
 
-export default CleanupFunction;
+const SecondComponent = () => {
+  useEffect(() => {
+    console.log('useEffect invoked')
+  }, [])
+
+  return <p>Second Component Toggled</p>
+}
+
+const CleanupFunction = () => {
+  const [value, setValue] = useState(true)
+
+  return (
+    <>
+      <h2>cleanup function</h2>
+      <button type="button" className="btn" onClick={() => setValue(!value)}>
+        Toggle State
+      </button>
+      {value && <SecondComponent />}
+    </>
+  )
+}
+
+export default CleanupFunction
