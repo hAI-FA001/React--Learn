e7709985e3e076d21232c23ebdd280da31bff68d
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-09 09:25:54 +0500

    short circuit examples + default value + conditionally add component or tags

--- Advanced/src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
+++ Advanced/src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
@@ -1,14 +1,33 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 const ShortCircuitExamples = () => {
   // falsy
-  const [text, setText] = useState('');
+  const [text, setText] = useState('')
   // truthy
-  const [name, setName] = useState('susan');
-  const [user, setUser] = useState({ name: 'john' });
-  const [isEditing, setIsEditing] = useState(false);
+  const [name, setName] = useState('susan')
+  const [user, setUser] = useState({ name: 'john' })
+  const [isEditing, setIsEditing] = useState(false)
 
-  return <h2>short circuit - examples</h2>;
-};
+  return (
+    <>
+      <h2>short circuit - examples</h2>
+      <h3>{text || 'a default value'}</h3>
+      {text && (
+        <div>
+          <h3>{name}</h3>
+        </div>
+      )}
 
-export default ShortCircuitExamples;
+      {user && <SomeCompo {...user} />}
+    </>
+  )
+}
+
+const SomeCompo = ({ name }) => (
+  <div>
+    <h2>Component</h2>
+    <h3>{name}</h3>
+  </div>
+)
+
+export default ShortCircuitExamples
