62e4f88b588adc5d37f669b6ae820b7f588f3a3a
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-09 11:26:59 +0500

    project structure - named export + index.jsx to simplify

--- Advanced/src/App.jsx
+++ Advanced/src/App.jsx
@@ -1,5 +1,4 @@
-import About from './tutorial/04-project-structure/starter/Pages/About'
-import Home from './tutorial/04-project-structure/starter/Pages/Home'
+import { About, Home } from './tutorial/04-project-structure/starter/Pages'
 
 function App() {
   return (
