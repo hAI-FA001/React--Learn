e1fcfe81b99b7cfde9c0a69261ad1efa31dc6468
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-11 10:26:37 +0500

    add user challenge + controlled input

--- Advanced/src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ Advanced/src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,21 +1,43 @@
+import { useState } from 'react'
+import { data } from '../../../data'
+
 const UserChallenge = () => {
+  const [name, setName] = useState('')
+  const [people, setPeople] = useState(data)
+
   return (
     <div>
-      <form className='form'>
+      <form
+        className="form"
+        onSubmit={(e) => {
+          e.preventDefault()
+          setPeople(people.concat([{ id: people.length + 1, name: name }]))
+        }}
+      >
         <h4>Add User</h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input
+            type="text"
+            className="form-input"
+            id="name"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value)
+            }}
+          />
         </div>
 
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
-      {/* render users below */}
+      {people.map((p) => (
+        <div key={p.id}>{p.name}</div>
+      ))}
     </div>
-  );
-};
-export default UserChallenge;
+  )
+}
+export default UserChallenge
