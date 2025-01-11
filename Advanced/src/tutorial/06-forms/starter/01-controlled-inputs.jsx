9c6277a552cfbc94ea6a12c854934773fde9a811
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-11 10:21:18 +0500

    controlled inputs

--- Advanced/src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ Advanced/src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -1,19 +1,47 @@
+import { useState } from 'react'
+
 const ControlledInputs = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+
   return (
-    <form className="form" action="">
+    <form
+      className="form"
+      action=""
+      onSubmit={(e) => {
+        e.preventDefault()
+        console.log(name, email)
+      }}
+    >
       <h2>Controlled Inputs</h2>
       <div className="form-row">
         <label htmlFor="name" className="form-label">
           name
         </label>
-        <input type="text" id="name" className="form-input" />
+        <input
+          type="text"
+          id="name"
+          className="form-input"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value)
+          }}
+        />
       </div>
 
       <div className="form-row">
         <label htmlFor="email" className="form-label">
           email
         </label>
-        <input type="text" id="email" className="form-input" />
+        <input
+          type="text"
+          id="email"
+          className="form-input"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value)
+          }}
+        />
       </div>
 
       <button type="submit" className="btn btn-block">
