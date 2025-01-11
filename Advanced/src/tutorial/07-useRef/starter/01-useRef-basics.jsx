ab0ea149642e4483acf8c093389fa39ccfc8d62c
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-11 11:17:23 +0500

    useRef for 1 uncontrolled input

--- Advanced/src/tutorial/07-useRef/starter/01-useRef-basics.jsx
+++ Advanced/src/tutorial/07-useRef/starter/01-useRef-basics.jsx
@@ -1,31 +1,47 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react'
 
 const UseRefBasics = () => {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(0)
+  const refContainer = useRef(null)
+
+  console.log(refContainer)
+
+  // after initial render, we can access input using refContainer
+  useEffect(() => {
+    console.log(refContainer)
+  })
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+    e.preventDefault()
+
+    console.log(refContainer.current.value)
+  }
 
   return (
     <div>
-      <form className='form' onSubmit={handleSubmit}>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             Name
           </label>
-          <input type='text' id='name' className='form-input' />
+          <input
+            type="text"
+            id="name"
+            className="form-input"
+            ref={refContainer}
+          />
         </div>
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
+
       <h1>value : {value}</h1>
-      <button onClick={() => setValue(value + 1)} className='btn'>
+      <button onClick={() => setValue(value + 1)} className="btn">
         increase
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default UseRefBasics;
+export default UseRefBasics
